a017e3be3268eabed99a460563d58808de441611
Ayumu Saito <d120145@Ayumus-Mac-mini.local>
2023-02-18 21:21:34 +0900

    Setup for Routing by pages directory

---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<script lang="tsx">
+  export default defineComponent({
+    render(){
+      return (
+        <div>
+          <p>Hello World!</p>
+        </div>
+      )
+    }
+  })
+</script>
