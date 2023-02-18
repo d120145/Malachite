35cb4e7100be03ee8501de58e2a73a077a8f949a
d120145 <dr.ayumus@gmail.com>
2023-02-19 01:06:10 +0900

    Add Home List Contents

---
+++ components/HomeListContent.vue
@@ -0,0 +1,30 @@
+<script lang="tsx">import { css } from '@emotion/css';
+import { addSpace } from '~~/assets/rules/Rules';
+import HomeListItemVue from './HomeListItem.vue';
+
+export default defineComponent({
+    render(){
+
+        const homeListContentBodyStyle = css({
+            padding: `${addSpace(5)}px 0`,
+        })
+
+        const homeListContentListStyle = css({
+            display: 'flex',
+            gap: addSpace(2),
+            listStyleType: 'none',
+            "@media (max-width: 480px)":{
+                flexDirection: 'column',
+            }
+        })
+
+        return(
+            <section className={homeListContentBodyStyle}>
+                <ul className={homeListContentListStyle}>
+                    <HomeListItemVue />
+                </ul>
+            </section>
+        )
+    }
+})
+</script>
---
+++ components/HomeListItem.vue
@@ -0,0 +1,85 @@
+<script lang="tsx">
+import TsIcon from '~~/assets/images/image-typescript_logo.svg';
+import EmotionIcon from '~~/assets/images/image-emotion_logo.svg';
+import NetlifyIcon from '~~/assets/images/image-netlify_logo.svg';
+import { css } from '@emotion/css';
+import { addSpace, Colors, Display, Text, Weight } from '~~/assets/rules/Rules';
+
+export default defineComponent ({
+    render(){
+
+        const homeListItemStyle = css({
+            backgroundColor: Colors.WhitePrimary,
+            border: `1px solid ${Colors.BorderBasic}`,
+            borderRadius: 8,
+            display: 'grid',
+            gap: addSpace(1),
+            padding: addSpace(3),
+            width: `calc(100%/3 - 16px)`,
+            "@media (max-width: 480px)":{
+                width: Display.FullSize,
+            }
+        })
+
+        const homeListItemImageStyle = css({
+            display: 'block',
+            height: 48,
+            width: 48,
+        })
+
+        const homeListItemTitleStyle = css({
+            color: Colors.BlackPrimary,
+            fontSize: Text.Type3,
+            fontWeight: Weight.Bold,
+        })
+
+        const homeListItemTextStyle = css({
+            color: Colors.GrayPrimary,
+            fontSize: Text.Type5,
+            fontWeight: Weight.Medium,
+        })
+
+        const ListItem:{
+            image:string,
+            title:string,
+            description:string,
+        }[] = [
+            {
+                image:TsIcon,
+                title: "Only TSX",
+                description: ".vue内は<template>と<style>を使用せずに<script>のみで記述しています。"
+            },
+            {
+                image:EmotionIcon,
+                title: "@emotion/css",
+                description: "@emotion/cssを採用し、tsx内でスタイリングしています。",
+            },
+            {
+                image: NetlifyIcon,
+                title: "Auto Deploy",
+                description: "MergeしたらNetlifyで自動的にデプロイされるようにしています。"
+            },
+        ]
+
+        return(
+            <>
+            {ListItem.map(item => {
+                return(
+                    <li className={homeListItemStyle}>
+                        <img
+                        alt=''
+                        className={homeListItemImageStyle}
+                        src={item.image}
+                        height={48}
+                        width={48}
+                        />
+                        <h2 className={homeListItemTitleStyle}>{item.title}</h2>
+                        <p className={homeListItemTextStyle}>{item.description}</p>
+                    </li>
+                )
+            })}
+            </>
+        )
+    }
+})
+</script>
