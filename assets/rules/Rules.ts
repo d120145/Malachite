62b21add10a1d42981adaf050bcf5be7bc457db8
d120145 <dr.ayumus@gmail.com>
2023-02-18 21:50:10 +0900

    Add Rules.ts

---
+++ assets/rules/Rules.ts
@@ -0,0 +1,45 @@
+export const spaceBaseSize = 8
+export const addSpace = (size:number) => spaceBaseSize * size
+
+export const Text = {
+    Type1: '32px',
+    Type2: '24px',
+    Type3: '20px',
+    Type4: '16px',
+    Type5: '14px',
+}
+
+export const Weight = {
+    Bold: '600',
+    Medium: '500',
+}
+
+export const Hover = {
+    Transition: '.2s cubic-bezier(0.45, 0, 0.55, 1)',
+}
+
+export const Display = {
+    FullSize: '100%',
+    ContentSize: '640px',
+}
+
+export const Colors = {
+    BackgroundLight: '#F9FBFC',
+    BorderBasic: '#5c93bb2b',
+    BlackPrimary: '#191C1F',
+    CharcoalPrimary: '#12161c',
+    GrayPrimary: '#505A63',
+    GraySecondary: '#BDC3C7',
+    GrayTertiary: '#F2F2F2',
+    WhitePrimary: '#FFFFFF',
+    
+    // System Color
+    SystemRed: '#FF3B30',
+    SystemOrange: '#FF9500',
+    SystemYellow: '#FFCC00',
+    SystemGreen: '#34C759',
+    SystemCyan: '#64D2F6',
+    SystemBlue: '#007AFF',
+    SystemIndigo: '#5856D6',
+    SystemPurple: '#AF52DE',
+}
