82b2addcc7dca9ef1580859d5c8ab8c69ac64ab4
d120145 <dr.ayumus@gmail.com>
2023-02-18 23:37:35 +0900

    fixup! Add Main component

--- components/templates/Main.vue
+++ components/templates/Main.vue
@@ -1,13 +1,18 @@
 <script lang="tsx">
 import { css } from '@emotion/css';
-import { Display } from '~~/assets/rules/Rules';
+import { addSpace, Display } from '~~/assets/rules/Rules';
 
 export default defineComponent({
     setup(_, { slots }){
 
         const mainStyle = css ({
-            margin: `0 ${'auto'}`,
+            margin: `${addSpace(7)}px ${'auto'}`,
             width: Display.ContentSize,
+            "@media (max-width: 480px)":{
+                margin: `${addSpace(6)}px 0`,
+                padding: `0 ${addSpace(3)}px`,
+                width: Display.FullSize,
+            }
         })
 
         return () => (
