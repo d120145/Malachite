24d69248e378b0d324375adbad2adb1b99f172e2
d120145 <dr.ayumus@gmail.com>
2023-02-18 23:05:27 +0900

    Add Main component

---
+++ components/templates/Main.vue
@@ -0,0 +1,20 @@
+<script lang="tsx">
+import { css } from '@emotion/css';
+import { Display } from '~~/assets/rules/Rules';
+
+export default defineComponent({
+    setup(_, { slots }){
+
+        const mainStyle = css ({
+            margin: `0 ${'auto'}`,
+            width: Display.ContentSize,
+        })
+
+        return () => (
+            <main className={mainStyle}>
+                {slots.default && slots.default()}
+            </main>
+        )
+    }
+})
+</script>
