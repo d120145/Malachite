e66d9227d8f1701875f69b72fceeee2b8ec51539
d120145 <dr.ayumus@gmail.com>
2023-02-19 01:52:08 +0900

    Add Footer

---
+++ components/Footer.vue
@@ -0,0 +1,29 @@
+<script lang="tsx">
+import { css } from '@emotion/css';
+import { addSpace, Colors, Text, Weight } from '~~/assets/rules/Rules';
+
+export default defineComponent({
+    render(){
+
+        const footerBodyStyle = css({
+            backgroundColor: Colors.WhitePrimary,
+            borderTop: `1px solid ${Colors.BorderBasic}`,
+            display: 'flex',
+            justifyContent: 'center',
+            padding: addSpace(2),
+        })
+
+        const footerTextStyle = css({
+            color: Colors.GrayPrimary,
+            fontSize: Text.Type5,
+            fontWeight: Weight.Medium,
+        })
+
+        return(
+            <footer className={footerBodyStyle}>
+                <p className={footerTextStyle}>&copy; 2023- nuxt.ayumusaito.dev by AyumuSaito</p>
+            </footer>
+        )
+    }
+})
+</script>
