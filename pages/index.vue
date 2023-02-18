b31388e190772ca1bad9bb262228ec24f22946c1
d120145 <dr.ayumus@gmail.com>
2023-02-19 01:52:35 +0900

    Create Index Component

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,24 @@
 <script lang="tsx">
+import FooterVue from '~~/components/Footer.vue';
+import HeaderVue from '~~/components/Header.vue';
+import HomeGithubLinkContentVue from '~~/components/HomeGithubLinkContent.vue';
+import HomeListContentVue from '~~/components/HomeListContent.vue';
+import HomeTopContentVue from '~~/components/HomeTopContent.vue';
+import MainVue from '~~/components/templates/Main.vue';
 import  Helloworld  from '../components/Helloworld.vue'
 
   export default defineComponent({
     render(){
       return (
-        <Helloworld />
+        <>
+        <HeaderVue />
+        <MainVue>
+          <HomeTopContentVue />
+          <HomeListContentVue />
+          <HomeGithubLinkContentVue />
+        </MainVue>
+        <FooterVue />
+        </>
       )
     }
   })
